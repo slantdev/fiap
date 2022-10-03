9055e9b57c113f2dad6b211ecb5171f03a6561fc
Fransnico Susanto <fransnico@amazeworx.com>
2022-10-03 09:36:14 +0700

    update safari

--- header.php
+++ header.php
@@ -67,7 +67,7 @@
               )
             );
             ?>
-            <div class="py-3">
+            <div class="py-3 pl-6">
               <form class="relative">
                 <input type="text" placeholder="Search" class="rounded-md px-3 py-2.5 w-full min-w-[260px] border-transparent" />
                 <?php echo fiap_icon(array('icon' => 'search', 'group' => 'utility', 'size' => '20', 'class' => 'absolute right-3 top-3 text-fiap-dark-blue')) ?>
