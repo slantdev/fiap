51a7f7de453c78fff649cf6cc9cac1b05024a495
Fransnico Susanto <fransnico@amazeworx.com>
2022-10-14 09:27:07 +0700

    add image on card_numbers

--- template-parts/sections/card_numbers.php
+++ template-parts/sections/card_numbers.php
@@ -11,9 +11,14 @@ include get_template_directory() . '/template-parts/layouts/section_settings.php
 
 <section id="<?php echo $section_id ?>" style="<?php echo $section_style ?>">
   <div class="relative container mx-auto <?php echo $section_padding_top . ' ' . $section_padding_bottom ?>">
-    <div class="w-full">
-      <?php get_template_part('template-parts/components/heading', '', array('field' => 'heading', 'class' => 'w-2/3')); ?>
-      <?php get_template_part('template-parts/components/description', '', array('field' => 'description', 'class' => 'max-w-none xl:prose-lg')); ?>
+    <div class="w-full flex flex-wrap lg:flex-nowrap lg:gap-x-14">
+      <div class="w-full lg:w-2/3">
+        <?php get_template_part('template-parts/components/heading', '', array('field' => 'heading', 'class' => 'w-full')); ?>
+        <?php get_template_part('template-parts/components/description', '', array('field' => 'description', 'class' => 'max-w-none xl:prose-lg')); ?>
+      </div>
+      <div class="w-full lg:w-1/3">
+        <?php get_template_part('template-parts/components/image', '', array('class' => 'aspect-square object-cover')); ?>
+      </div>
     </div>
     <?php get_template_part('template-parts/components/card_numbers', '', array('field' => 'cards_number_repeater', 'class' => '')); ?>
   </div>
