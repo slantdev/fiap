27df0d5cbf763cfbde0195d8a75fa69451b10762
Fransnico Susanto <fransnico@amazeworx.com>
2022-11-24 10:04:28 +0700

    update register template

--- templates/template-register.php
+++ templates/template-register.php
@@ -13,6 +13,7 @@ get_template_part('template-parts/layouts/page-header', '', array('breadcrumbs' 
 while (have_posts()) :
   the_post();
   echo '<div class="pt-12 lg:pt-20 xl:pt-20 pb-12 lg:pb-20 xl:pb-20 relative container mx-auto">';
+  echo '<h1 class="text-3xl lg:text-[34px] leading-tight font-semibold mb-6 lg:mb-12">' . get_the_title() . '</h1>';
   echo '<div class="prose xl:prose-lg  mb-6 xl:mb-8">';
   the_content();
   echo '</div>';
